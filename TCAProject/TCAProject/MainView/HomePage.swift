f9f6984ad71f97f456f845d338ae8ca92a044d0e
Taeangel <wngks2050@gmail.com>
2022-12-02 02:48:30 +0900

    feat: ContentView를 HomePage 파일로 변경

---
+++ TCAProject/TCAProject/MainView/HomePage.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  TCAProject
+//
+//  Created by song on 2022/12/02.
+//
+import SwiftUI
+import LinkNavigator
+
+struct HomePage: View {
+  let navigator: LinkNavigatorType
+
+  var body: some View {
+    
+    Text("asdfjkahsdf")
+    
+  }
+}
