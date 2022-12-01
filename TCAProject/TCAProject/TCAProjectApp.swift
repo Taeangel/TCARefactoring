c59cdfc7aa34843f581f1099173d7266458500a0
Taeangel <wngks2050@gmail.com>
2022-12-02 02:25:22 +0900

    feat: 프로잭트 생성

---
+++ TCAProject/TCAProject/TCAProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAProjectApp.swift
+//  TCAProject
+//
+//  Created by song on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct TCAProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
