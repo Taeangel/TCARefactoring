b4edad4e1ed295a1b92ac8822f618975e6887e45
Taeangel <wngks2050@gmail.com>
2022-12-02 02:32:33 +0900

    feat:  AppDelegate 파일 생성

---
+++ TCAProject/TCAProject/LinkNavigator/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TCAProject
+//
+//  Created by song on 2022/12/02.
+//
+
+import SwiftUI
+import LinkNavigator
+
+final class AppDelegate: NSObject {
+  var navigator: LinkNavigator {
+    LinkNavigator(dependency: AppDependency(), builders: AppRouterGroup().routers)
+  }
+}
+
+extension AppDelegate: UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+    true
+  }
+}
