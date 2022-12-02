fa0b35f676ca89464c6184f79e9c785e449d7288
Taeangel <wngks2050@gmail.com>
2022-12-02 20:13:56 +0900

    feat: AppSideEffect 생성

--- TCAProject/TCAProject/LinkNavigator/AppSideEffect.swift
+++ TCAProject/TCAProject/LinkNavigator/AppSideEffect.swift
@@ -6,5 +6,27 @@
 //
 
 import LinkNavigator
+import Dependencies
+import Foundation
 
-struct AppDependency: DependencyType { }
+
+struct AppDependency: DependencyType {}
+
+fileprivate var navigator: LinkNavigatorType = LinkNavigator(
+  dependency: AppDependency(),
+  builders: AppRouterGroup().routers)
+
+public struct AppSideEffect: DependencyKey {
+  
+  let linkNavigator: LinkNavigatorType
+  
+  public static var liveValue: AppSideEffect {
+    return .init(linkNavigator: navigator)
+  }
+}
+
+extension DependencyValues {
+  var sideEffect: AppSideEffect {
+    get {self[AppSideEffect.self] }
+  }
+}
