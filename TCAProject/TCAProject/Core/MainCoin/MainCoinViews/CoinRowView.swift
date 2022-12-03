84ad4939cadc7a6df799cee417ef7e776e46fff1
Taeangel <wngks2050@gmail.com>
2022-12-03 20:22:45 +0900

    feat: CoinRowView 생성

---
+++ TCAProject/TCAProject/Core/MainCoin/MainCoinViews/CoinRowView.swift
@@ -0,0 +1,24 @@
+//
+//  CoinRowView.swift
+//  TCAProject
+//
+//  Created by song on 2022/12/03.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct CoinRowView: View {
+  
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+
+
+struct CoinRowView_Previews: PreviewProvider {
+  static var previews: some View {
+    CoinRowView()
+  }
+}
