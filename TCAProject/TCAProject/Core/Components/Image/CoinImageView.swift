da738d232de72a90eb913bee15582d468fd13966
Taeangel <wngks2050@gmail.com>
2022-12-03 20:38:01 +0900

    feat: CoinImageView 구현

---
+++ TCAProject/TCAProject/Core/Components/Image/CoinImageView.swift
@@ -0,0 +1,20 @@
+//
+//  CoinImageView.swift
+//  TCAProject
+//
+//  Created by song on 2022/12/03.
+//
+
+import SwiftUI
+
+struct CoinImageView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct CoinImageView_Previews: PreviewProvider {
+  static var previews: some View {
+    CoinImageView()
+  }
+}
